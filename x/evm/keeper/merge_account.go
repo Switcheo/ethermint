9ce6cab6a6279f0b61f0fb3de5e1c9d1e0ed6e5a
Randy <randy75828@gmail.com>
2022-12-14 16:15:21 +0800

    [WIP] add placeholder for merge account method in evm keeper to satisfy interface definition

---
+++ x/evm/keeper/merge_account.go
@@ -0,0 +1,7 @@
+package keeper
+
+import sdk "github.com/cosmos/cosmos-sdk/types"
+
+func (k *Keeper) AccountHasAlreadyBeenMerged(ctx sdk.Context, address sdk.AccAddress, isEthAddress bool) bool {
+	panic("Not implemented!")
+}
