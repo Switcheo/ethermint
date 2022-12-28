62157e8722128905b09fa17fe3327befc1d8da82
Randy <randy75828@gmail.com>
2022-12-28 17:15:54 +0800

    fix generation of addresses to use getfromBech32 instead due to config seal issues

--- app/utils.go
+++ app/utils.go
@@ -36,18 +36,17 @@ var (
 )
 
 func init() {
-	setupConfig()
 	generateAddresses()
 }
 
 func generateAddresses() {
 	//addresses use for testing
-	Maker1 = sdk.MustAccAddressFromBech32("ethm1d9wzmf532988dcehds2h2cw9ds93xuka8rd67k")
-	Maker2 = sdk.MustAccAddressFromBech32("ethm12e3xkjythwaj8yd0fkm3hkdexhvkngw3fma6rk")
-	Maker3 = sdk.MustAccAddressFromBech32("ethm18wysn7mm0w7ca0tkgvnh5hrff9ymysayjq38f2")
-	Maker4 = sdk.MustAccAddressFromBech32("ethm1wx7sfduqz0utwuneg0gwvv4ptmzs7gqrzwtfup")
-	Maker5 = sdk.MustAccAddressFromBech32("ethm195gwa7l29c0gycm9gk6qug3hqfhf7dsktvj8cy")
-	Maker6 = sdk.MustAccAddressFromBech32("ethm1x2vk3d8m4lw7065ysryd5z5j4nvy5qc9ga8x2k")
+	Maker1, _ = sdk.GetFromBech32("ethm1d9wzmf532988dcehds2h2cw9ds93xuka8rd67k", "ethm")
+	Maker2, _ = sdk.GetFromBech32("ethm12e3xkjythwaj8yd0fkm3hkdexhvkngw3fma6rk", "ethm")
+	Maker3, _ = sdk.GetFromBech32("ethm18wysn7mm0w7ca0tkgvnh5hrff9ymysayjq38f2", "ethm")
+	Maker4, _ = sdk.GetFromBech32("ethm1wx7sfduqz0utwuneg0gwvv4ptmzs7gqrzwtfup", "ethm")
+	Maker5, _ = sdk.GetFromBech32("ethm195gwa7l29c0gycm9gk6qug3hqfhf7dsktvj8cy", "ethm")
+	Maker6, _ = sdk.GetFromBech32("ethm1x2vk3d8m4lw7065ysryd5z5j4nvy5qc9ga8x2k", "ethm")
 }
 
 //Add setupConfig here to initialise addresses
