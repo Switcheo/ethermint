3d780a6e1bb51961960cbd920c12930d962b9e88
wr1159 <chuweiron18@gmail.com>
2024-09-25 23:16:27 +0800

    Refactor getChainIDFromMemo and add carbon chain check

--- ethereum/eip712/encoding.go
+++ ethereum/eip712/encoding.go
@@ -24,6 +24,7 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	txTypes "github.com/cosmos/cosmos-sdk/types/tx"
+	evmkeeper "github.com/evmos/ethermint/x/evm/keeper"
 
 	sdktestutil "github.com/cosmos/cosmos-sdk/types/module/testutil"
 	apitypes "github.com/ethereum/go-ethereum/signer/core/apitypes"
@@ -85,6 +86,31 @@ func isValidEIP712Payload(typedData apitypes.TypedData) bool {
 	return len(typedData.Message) != 0 && len(typedData.Types) != 0 && typedData.PrimaryType != "" && typedData.Domain != apitypes.TypedDataDomain{}
 }
 
+// getChainIDFromMemo extracts the signed chain ID from the memo field of a cross chain signing transaction.
+// This is for cross-chain EIP712.
+func getChainIDFromMemo(memo string) (string, error) {
+	splitMemoCrossChain := strings.Split(memo, "|CROSSCHAIN-SIGNING|")
+	if len(splitMemoCrossChain) != 2 {
+		return "", errors.New("invalid memo")
+	}
+	memoSuffix := splitMemoCrossChain[1]
+	memoChainIDs := strings.Split(memoSuffix, ";")
+
+	if len(memoChainIDs) != 2 {
+		return "", errors.New("invalid memo")
+	}
+	signedChainSplit := strings.Split(memoChainIDs[0], ":")
+	carbonChainSplit := strings.Split(memoChainIDs[1], ":")
+	if len(signedChainSplit) != 2 || len(carbonChainSplit) != 2 {
+		return "", errors.New("invalid memo")
+	}
+	// Check the carbon chain ID to prevent replay attack
+	if carbonChainSplit[1] != evmkeeper.EvmChainId {
+		return "", fmt.Errorf("invalid chainId, expected %v, got %v", evmkeeper.EvmChainId, carbonChainSplit[1])
+	}
+	return signedChainSplit[1], nil
+}
+
 // decodeAminoSignDoc attempts to decode the provided sign doc (bytes) as an Amino payload
 // and returns a signable EIP-712 TypedData object.
 func decodeAminoSignDoc(signDocBytes []byte) (apitypes.TypedData, error) {
@@ -120,22 +146,11 @@ func decodeAminoSignDoc(signDocBytes []byte) (apitypes.TypedData, error) {
 	// Cross chain signing portion to convert aminoDoc.ChainId to the chainID of the signed chain
 	memo := aminoDoc.Memo
 	if strings.Contains(memo, "|CROSSCHAIN-SIGNING|") {
-		splitMemoCrossChain := strings.Split(memo, "|CROSSCHAIN-SIGNING|")
-		if len(splitMemoCrossChain) != 2 {
-			return apitypes.TypedData{}, errors.New("invalid memo")
+		signedChainID, err := getChainIDFromMemo(memo)
+		if err != nil {
+			return apitypes.TypedData{}, err
 		}
-		memoSuffix := splitMemoCrossChain[1]
-		memoChainIDs := strings.Split(memoSuffix, ";")
-
-		if len(memoChainIDs) != 2 {
-			return apitypes.TypedData{}, errors.New("invalid memo")
-		}
-		signedChainSplit := strings.Split(memoChainIDs[0], ":")
-		carbonChainSplit := strings.Split(memoChainIDs[1], ":")
-		if len(signedChainSplit) != 2 || len(carbonChainSplit) != 2 {
-			return apitypes.TypedData{}, errors.New("invalid memo")
-		}
-		aminoDoc.ChainID = signedChainSplit[1]
+		aminoDoc.ChainID = signedChainID
 	}
 
 	chainID, err := types.ParseChainID(aminoDoc.ChainID)
@@ -220,22 +235,11 @@ func decodeProtobufSignDoc(signDocBytes []byte) (apitypes.TypedData, error) {
 	// Cross chain signing portion to convert signDoc.ChainId to the chainID of the signed chain
 	memo := body.Memo
 	if strings.Contains(memo, "|CROSSCHAIN-SIGNING|") {
-		splitMemoCrossChain := strings.Split(memo, "|CROSSCHAIN-SIGNING|")
-		if len(splitMemoCrossChain) != 2 {
-			return apitypes.TypedData{}, errors.New("invalid memo")
+		signedChainID, err := getChainIDFromMemo(memo)
+		if err != nil {
+			return apitypes.TypedData{}, err
 		}
-		memoSuffix := splitMemoCrossChain[1]
-		memoChainIDs := strings.Split(memoSuffix, ";")
-
-		if len(memoChainIDs) != 2 {
-			return apitypes.TypedData{}, errors.New("invalid memo")
-		}
-		signedChainSplit := strings.Split(memoChainIDs[0], ":")
-		carbonChainSplit := strings.Split(memoChainIDs[1], ":")
-		if len(signedChainSplit) != 2 || len(carbonChainSplit) != 2 {
-			return apitypes.TypedData{}, errors.New("invalid memo")
-		}
-		signDoc.ChainId = signedChainSplit[1]
+		signDoc.ChainId = signedChainID
 	}
 
 	chainID, err := types.ParseChainID(signDoc.ChainId)
